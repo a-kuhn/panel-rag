2b917c7caa6a942a6a53db5a88552e24470c5ec9
a-kuhn <alex.kuhn1123@gmail.com>
2024-07-23 13:59:29 -0400

    working mvp

--- src/app.py
+++ src/app.py
@@ -1,21 +1,31 @@
 import panel as pn
 from retrieve_docs import retrieve_docs
 from augment_prompt import build_prompt
-from generate_response import generate_response
+from generate_response import generate_openai_response
 
 pn.extension()
 
-def get_response(contents, user, instance):
-    docs = retrieve_docs(contents)
-    prompt = build_prompt(docs)
-    response = generate_response(prompt)
 
-    if "turbine" in contents.lower():
-        response = "A wind turbine converts wind energy into electricity."
-    else:
-        response = "Sorry, I don't know."
+def get_response(contents, user, instance):
+    docs, _ = retrieve_docs(contents)
+    prompt = build_prompt(question=contents, context_docs=docs)
+    response = generate_openai_response(prompt)
+    print("\n********************************")
+    print(f"found docs from elasticsearch: {docs}\n\nand their scores: {_}")
+    print("\n********************************")
+    print(f"submitting prompt to openai: {prompt}")
+    print("\n********************************")
+    print(f"openai response: {response}")
     return response
 
-chat_bot = pn.chat.ChatInterface(callback=get_response, max_height=500)
-chat_bot.send("Ask me what a wind turbine is", user="Assistant", respond=False)
+
+chat_bot = pn.chat.ChatInterface(
+    callback=get_response,
+    max_height=500,
+    user="student",
+    show_undo=False,
+)
+chat_bot.send(
+    "Ask me about the Zoomcamp courses", respond=False, user="Zoomcamp TA", avatar="🚀"
+)
 chat_bot.servable()
--- src/augment_prompt.py
+++ src/augment_prompt.py
@@ -1,4 +1,33 @@
-prompt_template = ""
+def build_prompt(question: str, context_docs: list) -> str:
+    context = build_context(context_docs)
+    prompt = f"""
+You're a course teaching assistant. Answer the user QUESTION based on CONTEXT - the documents retrieved from our FAQ database. 
+Only use the facts from the CONTEXT. If the CONTEXT doesn't contain the answer, return "NONE"
 
-def build_prompt(docs: list, template: str = prompt_template) -> str:
-    pass
+QUESTION: {question}
+
+CONTEXT:
+{context}
+""".strip()
+
+    return prompt
+
+
+def build_context(context_docs: list) -> str:
+    context = ""
+
+    if not isinstance(context_docs, list):
+        return "Error: context_docs should be a list."
+
+    for doc in context_docs:
+        if not isinstance(doc, dict):
+            return "Error: Each item in context_docs should be a dictionary."
+
+        if not all(key in doc for key in ["section", "question", "text"]):
+            return "Error: Each dictionary should contain 'section', 'question', and 'text' keys."
+
+        context += f"Section: {doc['section']}\n"
+        context += f"Question: {doc['question']}\n"
+        context += f"Answer: {doc['text']}\n\n"
+
+    return context.strip()
